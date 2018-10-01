2c5b383977f18ad9c9b507b531f5247d473f4945
Rajat Juneja <32433607+Rajat-Juneja@users.noreply.github.com>
2018-10-02 02:07:52 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
-require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
 const socket = require('socket.io');
-
-const news = require('./routes/api/news');
-
-const app = express();
-
-mongoose.connect('mongodb://localhost:27017/minor-news-app', { useNewUrlParser: true });
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-    console.log('Connected to Database');
-});
+var app = express();
 
 app.use(express.static('public'));
 users=[];
 connections=[];
 
-app.use('/api/news', news); 
-
-var server = app.listen(process.env.PORT||5000, () => {
+var server = app.listen(process.env.PORT||5000,()=>{
     console.log(server.address().port);
 });
 
@@ -29,17 +14,14 @@ const io = socket(server);
 
 io.sockets.on('connection',function(socket){
     connections.push(socket);
-    console.log("Connected are "+connections.length);
 
     socket.on('disconnect',function(data){
         users.splice(users.indexOf(socket.username),1);
         connections.splice(connections.indexOf(socket,1));
-        console.log("Disconnected , Total connections left are "+connections.length);
         io.sockets.emit('disconnected',users);
     })
 
     socket.on('send message',function(data){
-        console.log(data);
         io.sockets.emit('new message',{msg:data,user:socket.username});
     })
 
